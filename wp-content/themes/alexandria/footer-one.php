6d3a1f321b39e4f67c7f12715125fab4bb911fc1
Dimonze <dimonze@gmail.com>
2014-05-10 14:51:21 +0300

    updt

---
+++ wp-content/themes/alexandria/footer-one.php
@@ -0,0 +1,62 @@
+	<footer id="colophon" class="site-footer" role="contentinfo">
+    
+    	<div class="responsive-container">
+            	
+            <div class="site-info">           
+            
+               
+                <h3><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo('name'); ?></a></h3>
+                <p><?php _e('&copy; All rights reserved.', 'FDT STUDIO') ?></p>
+                <?php if( is_home() || is_front_page() ): ?>
+                <p><?php printf( __( 'Designed by: %1$s.', 'FDT STUDIO' ), '<a href="http://www.fdt.com/" rel="designer">FDT STUDIO</a>' ); ?></p>
+                <?php endif; ?>
+                <p>Powered by <a href="http://www.fdt.com/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'FDT STUDIO' ); ?>" rel="generator"><?php printf( __( '%s', 'dimonze' ), 'FDT STUDIO' ); ?></a></p>
+                <div class="footer-search"><?php //get_search_form(); ?></div>
+
+                
+            </div><!-- .site-info -->
+            
+            <div class="footer-widget-three">
+            	<?php if ( dynamic_sidebar('footer-left') ){ } else { ?>
+                
+                    <aside id="archives" class="widget">
+                        <h1 class="widget-title"><?php _e( 'Archives', 'dimonze' ); ?></h1>
+                        <ul>
+                            <?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+                        </ul>
+                    </aside>                                                                                
+                                                                                
+                <?php } ?>
+            </div>
+            
+            <div class="footer-widget-three">
+            	<?php if ( dynamic_sidebar('footer-center') ){ } else { ?>
+
+                    <aside id="meta" class="widget">
+                        <h1 class="widget-title"><?php _e( 'Meta', 'dimonze' ); ?></h1>
+                        <ul>
+                            <?php wp_register(); ?>
+                            <li><?php wp_loginout(); ?></li>
+                            <?php wp_meta(); ?>
+                        </ul>
+                    </aside>                                                                                
+                                                                                
+                <?php } ?>            
+            </div>
+            
+            <div class="footer-widget-three">
+            	<?php if ( dynamic_sidebar('footer-right') ){ } else { ?>
+
+                    <aside id="archives" class="widget">
+                        <h1 class="widget-title"><?php _e( 'Archives', 'dimonze' ); ?></h1>
+                        <ul>
+                            <?php wp_list_pages('title_li='); ?>
+                        </ul>
+                    </aside>                                                                                 
+                                                                                
+                <?php } ?>            
+            </div>            
+            
+    	</div><!-- #Responsive-Container -->
+                    
+	</footer><!-- #colophon -->
---
+++ wp-content/themes/alexandria/slider-cheader.php
@@ -0,0 +1,5 @@
+    <div id="feature" class="site-slider">   
+        
+        		<?php if(function_exists('vslider')){ vslider('mainslider'); } ?>                
+    
+    </div><!-- #banner -->
